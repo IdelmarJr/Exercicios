6467cac4bddceacc95dedd84a2d56d840dcd9c88
Idelmar Jr <idelmarjuniorre@gmail.com>
2024-12-09 19:48:10 -0300

    Atualizar o ex13.js

--- Cap 5/ex13.js
+++ Cap 5/ex13.js
@@ -1,12 +1,12 @@
 let n = Math.floor(Math.ramdom() * 101);
 let num = 0;
-for (let cont = 0; cont < 10; conta++) {
+for (let cont = 0; cont < 10; cont++) {
     num = parseInt(prompt('Digite um numero: '));
     if(n === num) {
         console.log('Parabens você acertou!');
         console.log(n);
     } else {
-        if (num > n){
+        if (num > n) {
             console.log('maior do que o numero secreto');
         } else {
             console.log('menor do que o numero secreto');
