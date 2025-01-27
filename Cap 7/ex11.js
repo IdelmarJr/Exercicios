297a21511ab994e4d8661436ecbb09a7d7035951
Idelmar Jr <idelmarjuniorre@gmail.com>
2025-01-27 19:35:04 -0300

    Add files via upload

---
+++ Cap 7/ex11.js
@@ -0,0 +1,13 @@
+let film = [];
+let coment = [];
+
+for (let i = 0; i < 5; i++){
+    let filme = prompt('Digite um filme: ');
+    let comentario = prompt('Digite um comentario: ');
+    film.push(filme);
+    coment.push(comentario);
+};
+
+film.forEach((filme, index)=>
+    console.log(film[index], 'seu comentario é ' + coment[index])
+);
